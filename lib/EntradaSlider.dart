b0bfeb95789566a95dd22602f03267b31782b245
Felipe Moreira <felipemoreirasilva97@gmail.com>
2021-03-17 23:55:35 -0300

    17/03/2021

---
+++ lib/EntradaSlider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class EntradaSlider extends StatefulWidget {
+  @override
+  _EntradaSliderState createState() => _EntradaSliderState();
+}
+
+class _EntradaSliderState extends State<EntradaSlider> {
+  double _valorUsuario = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrada de Dados"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(60),
+        child: Column(
+          children: <Widget>[
+            Slider(
+              value: _valorUsuario,
+              min: 0,
+              max: 10,
+              divisions: 10,
+              activeColor: Colors.red,
+              inactiveColor: Colors.deepPurple,
+              label: _valorUsuario.toString(),
+              onChanged: (double novoValor) {
+                setState(() {
+                  _valorUsuario = novoValor;
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                print("Valor Selecionado: " + _valorUsuario.toString());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:entrada_dados/EntradaRadioButton.dart';
 import 'EntradaCheckBox.dart';
 import 'package:entrada_dados/EntradaSwitch.dart';
+import 'package:entrada_dados/EntradaSlider.dart';
 
 void main() {
   runApp(MaterialApp(
     //home: CampoTexto(),
     //home: EntradaCheckBox(),
     //home: EntradaRadioButton(),
-    home: EntradaSwitch(),
+    //home: EntradaSwitch(),
+    home: EntradaSlider(),
   ));
 }
