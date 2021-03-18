e479a24a7a1d1b6602cf58b9320e35e26c04db7d
Felipe Moreira <felipemoreirasilva97@gmail.com>
2021-03-17 23:04:57 -0300

    17/03/2021

---
+++ lib/EntradaSwitch.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class EntradaSwitch extends StatefulWidget {
+  @override
+  _EntradaSwitchState createState() => _EntradaSwitchState();
+}
+
+class _EntradaSwitchState extends State<EntradaSwitch> {
+  bool _escolhaUsuario = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrada de Dados"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SwitchListTile(
+              title: Text("Receber notificações?"),
+              subtitle: Text("Ative para receber notificações"),
+              activeColor: Colors.green,
+              //selected: _escolhaUsuario,
+              value: _escolhaUsuario,
+              onChanged: (bool valor) {
+                setState(() {
+                  _escolhaUsuario = valor;
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                print("Resultado: " + _escolhaUsuario.toString());
+              },
+            ),
+
+            // Switch(
+            //   value: _escolhaUsuario,
+            //   onChanged: (bool valor) {
+            //     setState(() {
+            //       _escolhaUsuario = valor;
+            //     });
+            //   },
+            // ),
+            // Text("Receber notificações?"),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'CampoTexto.dart';
 import 'package:flutter/material.dart';
 import 'package:entrada_dados/EntradaRadioButton.dart';
 import 'EntradaCheckBox.dart';
+import 'package:entrada_dados/EntradaSwitch.dart';
 
 void main() {
   runApp(MaterialApp(
     //home: CampoTexto(),
     //home: EntradaCheckBox(),
-    home: EntradaRadioButton(),
+    //home: EntradaRadioButton(),
+    home: EntradaSwitch(),
   ));
 }
